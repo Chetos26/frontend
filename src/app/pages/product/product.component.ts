5e2273c1f3124469a5facfd2e14998e0f1654e6d
ChatonexX <98621169+ChatonexX@users.noreply.github.com>
2023-01-31 13:43:57 -0500

    .

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -7,23 +7,23 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implementsoninit{
-  constructor (private HttpClient:HttpClient) {
+  constructor (private HcttpClient:HttpClient) {
   }
-}
-ngOnInit(): void{
-  this.getProducts();
-}
-getProducts(){
-  this.HttpClient.get('https://api.escuela.co/api/v1/products')
-  console.console.log(Response);
+  ngOnInit(): void{
+    this.getProducts();
+  }
+  getProducts(){
+    this.HttpClient.get('https://api.escuela.co/api/v1/products')
+    console.console.log(Response);
+    
+  }
   
-}
-
-function ngOnInit() {
-  throw new Error('Function not implemented.');
-}
-
-function getProducts() {
-  throw new Error('Function not implemented.');
-}
+  function ngOnInit() {
+    throw new Error('Function not implemented.');
+  }
   
+  function getProducts() {
+    throw new Error('Function not implemented.');
+  }
+    
+}
