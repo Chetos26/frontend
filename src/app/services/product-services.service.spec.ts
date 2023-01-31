34675ed056003894240434466cfcbb0001396111
ChatonexX <leolmini_99@hotmail.com>
2023-01-30 19:10:53 -0500

    ,

--- src/app/services/product-services.service.spec.ts
+++ src/app/services/product-services.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProductServicesService } from './product.service';
+import { ProductService } from './product.service';
 
 describe('ProductService', () => {
-  let service: ProductServicesService;
+  let service: ProductService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductServicesService);
+    service = TestBed.inject(ProductService);
   });
 
   it('should be created', () => {
